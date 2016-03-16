8607a8bf1d4e9641d1f8010745f84b5d2bdb3c5c
wangxf <wangxf1988@gmail.com>
2016-03-16 13:48:59 +0800

    update

--- restfullframeworks/netty/src/main/java/cn/hz/netty/DiscardServerHandler.java
+++ restfullframeworks/netty/src/main/java/cn/hz/netty/DiscardServerHandler.java
@@ -1,10 +1,7 @@
 package cn.hz.netty;
 
-import io.netty.buffer.ByteBuf;
-
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.ReferenceCountUtil;
 
 /**
  * Handles a server-side channel.
